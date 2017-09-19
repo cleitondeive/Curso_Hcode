a167499457ef979e9cd4288c60c5367ffb9cd70c
cleitondeive <cleitondeive@gmail.com>
2017-09-18 22:35:39 -0400

    Apenas teste de arquivos local envio pasta compartilhada

---
+++ 01-Aula-Hcode-Variaveis.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Curso_Php_Hcode</title>
+
+	<!--Definição da viewport-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+
+	<!--Adicinar o Css do bootstrap-->
+	<link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
+
+	<!--Adicinar o Css personalizado-->
+	<link rel="stylesheet" href="css/estilo.css" media="screen">
+
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+
+</head>
+
+<body>
+
+
+<nav class="nav navbar-default">
+	<div class="container">
+		<div class="nav navbar-header">
+			<a class="navbar-brand" href="#">Curso_Php_Hcode</a>
+
+		</div>
+	</div>
+</nav>
+
+<div class="container">
+	<div class="colxs12">
+		<h1 class="h1-titulocor">Primeira_Aula-Curso_Php_Hcode_Variaveis</h1>
+	</div>
+
+</div>
+<hr>
+
+<!--inicio do Curso Php-->
+
+ <?php
+	$nome = "Cleiton";
+	$sobrenome = "Deive";
+
+	echo "$nome $sobrenome";
+
+
+   ?>
+
+
+
+
+
+
+<script src="js/jquery-3.1.1.min.js"></script> <!==Use o Script js/jquery no Final da pagina==>
+<script src="js/bootstrap.min.js"></script>	<!==Use o Script js/bootstrap no Final da pagina==>
+<script src="js/main.js"></script>
+
+
+
+
+
+
+</body>
+</html>
