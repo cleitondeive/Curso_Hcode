74ac34346aa4693f3ca52f417312eb646f106e69
cleitondeive <cleitondeive@gmail.com>
2017-09-29 18:41:49 -0400

    Terceira etapa do Projeto DAO carregando os metodos com lista

--- Dao-Pdo/index.php
+++ Dao-Pdo/index.php
@@ -58,12 +58,63 @@ echo json_encode($usuarios);
 
 //Carrega um usuário
 
-require_once("config.php");
 
+/* assim o select esta funcionando
 $sql = new Sql();
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo
 json_encode($usuarios);
+*/
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+//carrega um usuário usando o login e a senha
+//$usuario = new Usuario();
+//$usuario->login("root", "!@#$");
+//echo $usuario;
+/*
+//Criando um novo usuário
+$aluno = new Usuario("aluno", "@lun0");
+$aluno->insert();
+echo $aluno;
+*/
+/*
+//Alterar um usuário
+$usuario = new Usuario();
+$usuario->loadById(8);
+$usuario->update("professor", "!@#$%¨&*");
+echo $usuario;
+*/
+
+//carrega uma lista de usuarios
+/*
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+//carrega uma lista de usuarios buscando pelo Login
+
+
+
+//Carrega uma lista de usuários buscando pelo login por parte do nome
+
+//require_once("config.php");
+//$search = Usuario::search("ca");
+//echo json_encode($search);
+
+
+//carrega um usuário usando o login e a senha
+
+require_once("config.php");
+$usuario = new Usuario();
+$usuario->login("user_Fabio", "898989");
+echo $usuario;
 
  ?>
 
