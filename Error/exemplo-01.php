7902b94468144f646b791c268b851d2928dd4722
cleitondeive <cleitondeive@gmail.com>
2017-10-03 08:22:03 -0400

    Seção-15 Manipulando Tratamento de Erros com Php-07

---
+++ Error/exemplo-01.php
@@ -0,0 +1,21 @@
+
+  <?php
+  //Tratamentos de Erros PHP 7...
+  function error_handler($code,$message,$file,$line){
+    echo json_encode(array(
+      "code"=>$code,
+      "message"=>$message,
+      "line"=>$line,
+      "file"=>$file
+    ));
+
+  }
+
+  set_error_handler("error_handler");
+
+  echo 100 /0;
+
+
+
+
+  ?>
---
+++ Error/exemplo-02.php
@@ -0,0 +1,10 @@
+
+  <?php
+  //Tratamentos de Erros PHP 7...
+error_reporting(E_ALL & ~E_NOTICE);
+
+  $nome = $_GET["nome"];
+  echo "$nome";
+
+
+  ?>
